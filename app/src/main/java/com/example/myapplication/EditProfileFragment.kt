94fe960e4bfce04351bd8cdf6bd271ed160bc2d4
eunjin2118 <ally6324@naver.com>
2022-11-01 19:02:45 +0900

    edit profile 수정중

--- app/src/main/java/com/example/myapplication/EditProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/EditProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -34,20 +39,41 @@ class EditProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val btn_edit_profile = view.findViewById<Button>(R.id.btn_edit_profile)
+
+        val profile_name = view.findViewById<EditText>(R.id.profile_name)
+        val profile_phonenum = view.findViewById<EditText>(R.id.profile_phonenum)
+
+
         val currentUser = auth.currentUser
         var db = Firebase.firestore
 
-        val docRef = db.collection("wirtes").document(currentUser?.email.toString())
+        val docRef = db.collection("students").document(currentUser?.email.toString())
 
-        view.findViewById<TextView>(R.id.profile_email).text = docRef.toString()
+        btn_edit_profile.setOnClickListener {
+            db.collection("students").document(currentUser?.email.toString())
+                .update(mapOf(
+                    "nickname" to profile_name,
+                    "phonenum" to profile_phonenum
+
+                ))
+        }
+
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    view.findViewById<TextView>(R.id.profile_email).text =
+                        currentUser?.email.toString()
+                }
+            }
+            // 값이 없으면 널 리턴, 실패한거는
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "get failed with ", exception)
+            }
 
 
 
-//        docRef.get()
-//            .addOnSuccessListener { document ->
-//
-//            }
+
     }
-
 
 }
