f5163ee3fc169213de61849347c5a21a2d66b12c
eunjin2118 <ally6324@naver.com>
2022-11-29 20:06:07 +0900

    이미지 firestore에 올리기_진행중

--- app/src/main/java/com/example/myapplication/EditProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/EditProfileFragment.kt
@@ -1,24 +1,35 @@
 package com.example.myapplication
 
+import android.app.AlertDialog
+import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import com.example.myapplication.container.HomeContainerFragment
+import android.webkit.MimeTypeMap
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.myapplication.container.UserContainerFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 
 class EditProfileFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
+    private val storage = Firebase.storage
+    private var storageRef = storage.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +47,92 @@ class EditProfileFragment : Fragment() {
         return view
     }
 
+    fun showPermissionContextPopup() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("권한이 필요합니다")
+            .setMessage("전자액자에서 사진을 선택하려면 권한이 필요합니다.")
+            .setPositiveButton("동의하기", {_, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+            })
+            .setNegativeButton("취소하기",{ _,_ ->})
+            .create()
+            .show()
+    }
+    fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent,2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 2000) {
+            val selectedImageURI : Uri = data?.data!!
+            Log.d("mytag", selectedImageURI.toString())
+            val inputStream = requireActivity().contentResolver.openInputStream(selectedImageURI)!!
+
+            // val contentResolver: ContentResolver = requireActivity().contentResolver
+            // val mimeTypeMap = MimeTypeMap.getSingleton()
+            // val extension = mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(selectedImageURI))
+            // Log.d("mytag", extension.toString())
+            val fileRef = storageRef.child("${auth.currentUser!!.email}/profile")
+
+            // val stream = FileInputStream(File(selectedImageURI.path))
+            fileRef.putStream(inputStream).addOnSuccessListener {
+                Log.d("mytag", "success")
+            }
+
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            1000 -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // 권한이 부여 된 것입니다.
+                    // 허용 클릭 시
+                    navigatePhotos()
+                } else {
+                    // 거부 클릭시
+                    Toast.makeText(activity,"권한을 거부했습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val btnEditProfile = view.findViewById<Button>(R.id.btn_edit_profile)
+
+        val btnUploadProfile = view.findViewById<Button>(R.id.profile_upload_btn)
+        btnUploadProfile.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED -> {
+                    // READ_EXTERNAL_STORAGE의 권한이 PERMISSION_GRANTED와 같다면..
+                    //TODO 권한이 잘 부여되었을 때상황, 갤러리에서 사진을 선택하는 코드 구현
+                    navigatePhotos()
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    // 권한을 명시적으로 거부한 경우 true
+                    // 처음보거나, 다시묻지 않음을 선택한 경우 false
+                    //TODO 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
+                    showPermissionContextPopup()
+                }
+                else -> {
+                    // 처음봤을때 띄워줌
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+                }
+            }
+        }
 
         val profileName = view.findViewById<EditText>(R.id.profile_name)
         val profilePhone = view.findViewById<EditText>(R.id.profile_phone_num)
@@ -75,10 +168,5 @@ class EditProfileFragment : Fragment() {
             .addOnFailureListener { exception ->
                 Log.d(ContentValues.TAG, "get failed with ", exception)
             }
-
-
-
-
     }
-
 }
--- app/src/main/java/com/example/myapplication/UserFragment.kt
+++ app/src/main/java/com/example/myapplication/UserFragment.kt
@@ -2,21 +2,28 @@ package com.example.myapplication
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.example.myapplication.LoginActivity
+import androidx.fragment.app.Fragment
 import com.example.myapplication.container.UserContainerFragment
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ListResult
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import java.nio.ByteBuffer
+
 
 class UserFragment : Fragment() {
     private  lateinit var auth: FirebaseAuth
@@ -51,6 +58,11 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /// 스토리지 접근
+        val storage = Firebase.storage
+        // 스토리지 리퍼런스 객체 얻어오기 (이후 레퍼런스 객체를 이용하여 파일 업로드 다운로드 가능)
+        var storageRef = storage.reference
+
         val myWriteBtn = view.findViewById<Button>(R.id.my_write)
         val editProfile = view.findViewById<Button>(R.id.edit_profile)
 
@@ -61,6 +73,38 @@ class UserFragment : Fragment() {
 
         docRef.get()
             .addOnSuccessListener { document ->
+                val ref = storageRef.child(currentUser?.email.toString() + "/profile")
+                val localFile = File("temp", "profile")
+                // getFile 호출 이후 다운로드 시작
+                val downloadTask = ref.getFile(localFile)
+
+//                ref.listAll()
+//                    .addOnSuccessListener(OnSuccessListener<ListResult> { result ->
+//                        for (fileRef in result.items) {
+//                            ref.child("${fileRef.name}").downloadUrl.addOnCompleteListener {
+//                                if (it.isSuccessful) { // ALLIMGS는 key가 string, 값이 imageView인 해시맵이다.
+//                                    Glide.with(this).asBitmap().load(it.result)
+//                                        .into(object :BitmapImageViewTarget(AllIMGS["${fileRef.name}"]) {});
+//
+//                                }
+//                            }
+//                        }
+//                    })
+
+                downloadTask.addOnSuccessListener {
+
+                    // 3. 그 바이트를 Bitmap 이미지로 변환하고
+//                    val bytes = localFile.readBytes()
+////                    val bitmap = (imageView.getDrawable() as BitmapDrawable).bitmap
+//                    var decryptedText: ByteArray = cipher.doFinal(bytes)
+//                    val configBmp = Bitmap.Config.valueOf(bitmap.config.name)
+//                    val bitmap_tmp = Bitmap.createBitmap(width, height, configBmp)
+//                    val buffer = ByteBuffer.wrap(decryptedText)
+//                    bitmap_tmp.copyPixelsFromBuffer(buffer)
+
+                    // 4. 그 Bitmap을 ImageView로 소스로 설정하기
+                }
+
                 if (document != null) {
                     view.findViewById<TextView>(R.id.profile_name).text = document.data!!.get("nickname").toString()
                     view.findViewById<TextView>(R.id.profile_email).text = currentUser?.email.toString()
